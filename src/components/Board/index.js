409d20b55c2a54f1255e377c18e6d0273243e5c2
Adam Skinner <adam.skinner@gmail.com>
2016-10-29 19:55:10 -0400

    Sqaure to stateless functional component, state held in Board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Square from '../Square'
 export default class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />;
+    constructor(){
+        super();
+        this.state = {
+            squares: Array(9).fill(null)
+        };
     }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    }
+
     render() {
         const status = 'Next player: X';
         return (
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare(0)}
+                    {this.renderSquare()}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </div>
