e65c14073b78ef5cca68dd116495672ded68897d
Adam Skinner <adam.skinner@gmail.com>
2016-10-29 20:56:55 -0400

    Move state from Board to Game

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -2,40 +2,16 @@ import React from 'react';
 import Square from '../Square'
 
 export default class Board extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            squares: Array(9).fill(null),
-            playerToken: 'X',
-            winner: null
-        };
-    }
-
-    handleClick(i){
-        // If someone has already won the game or played in this square, ignore the click
-        if (this.state.winner != null || this.state.squares[i] != null)
-            return;
-
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.playerToken;
-        this.setState({
-            squares: squares,
-            playerToken: this.state.playerToken === 'X' ? 'O' : 'X',
-            winner: this.calculateWinner(squares)
-        });
-    }
-
-
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+        return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
     }
 
     render() {
         let status;
-        if (this.state.winner != null) {
-            status = `Winner: ${this.state.winner}`;
+        if (this.props.winner != null) {
+            status = `Winner: ${this.props.winner}`;
         } else {
-            status = `Next player: ${this.state.playerToken}`;
+            status = `Next player: ${this.props.playerToken}`;
         }
 
         return (
@@ -60,25 +36,4 @@ export default class Board extends React.Component {
         );
     }
 
-    calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-
-        return null;
-    }
 }
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import Board from '../Board'
 export default class Game extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            squares: Array(9).fill(null),
+            playerToken: 'X',
+            winner: null
+        };
+    }
+
+    handleClick(i){
+        // If someone has already won the game or played in this square, ignore the click
+        if (this.state.winner != null || this.state.squares[i] != null)
+            return;
+
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.playerToken;
+        this.setState({
+            history: this.state.history.concat([{
+                squares: squares
+            }]),
+            squares: squares,
+            playerToken: this.state.playerToken === 'X' ? 'O' : 'X',
+            winner: this.calculateWinner(squares)
+        });
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+
+        return null;
+    }
+
     render() {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board squares={this.state.squares}
+                           playerToken={this.state.playerToken}
+                           winner={this.state.winner}
+                           onClick={(i) => this.handleClick(i)} />
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
