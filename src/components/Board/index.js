635d46fd75de2233ec85bf07e4e9bb026838f920
Adam Skinner <adam.skinner@gmail.com>
2016-10-29 22:46:34 -0400

    Challenge 3: Abstract the rendering of rows

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -5,33 +5,34 @@ export default class Board extends React.Component {
     renderSquare(i) {
         return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
     }
-
+    renderRow(i){
+        let squareBaseline = i * 3; // 3 squares per row, starting with a zero index
+        let squares = [0, 1, 2].map(i => {
+           return this.renderSquare(squareBaseline + i);
+        });
+        return (
+            <div className="board-row" key={i}>
+                {squares}
+            </div>
+        )
+    }
     render() {
         let status;
+
         if (this.props.winner != null) {
             status = `Winner: ${this.props.winner}`;
         } else {
             status = `Next player: ${this.props.playerToken}`;
         }
 
+        let rows = [0, 1, 2].map(i => {
+           return this.renderRow(i);
+        });
+
         return (
             <div>
                 <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {rows}
             </div>
         );
     }
